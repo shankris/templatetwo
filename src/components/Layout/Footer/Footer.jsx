1d14280425b5088db7b4ae8519101e78c5707f54
Shankar Krishnasamy <shan_krishnasamy@yahoo.co.in>
2026-08-20 17:11:59 +0530

    Had missd the footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 // src/components/Layout/Footer/Footer.jsx
+"use client";
+
+import { useTranslations } from "next-intl";
 
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  const t = useTranslations("Footer");
+
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <span>Project Template</span>
-        <span>© 2026</span>
+        <span>{t("projectTemplate")}</span>
+        <span>{t("copyright")}</span>
       </div>
     </footer>
   );
